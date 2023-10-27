a87dbfc08f1bb8cf67116f4528558513bf1b6fcf
Marcin Rudowski <marcin.rudowski@fingo.info>
2023-10-27 11:54:44 +0200

    add runtime eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,18 +1,36 @@
 module.exports = {
   root: true,
   env: { browser: true, es2020: true },
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: ['./tsconfig.json', './tsconfig.node.json'],
+    tsconfigRootDir: __dirname,
+  },
   extends: [
     'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
+    // 'plugin:@typescript-eslint/recommended', replaced by:
+    'plugin:@typescript-eslint/recommended-type-checked',
     'plugin:react-hooks/recommended',
+    // 'plugin:@typescript-eslint/stylistic-type-checked', // Optionally
+    // 'plugin:import/recommended',
+    // 'plugin:import/typescript',
+
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
+    // 'plugin:testing-library/recommended',
+    // 'plugin:testing-library/react',
+    // 'plugin:jest-dom/recommended'
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  plugins: ['react-refresh', 'simple-import-sort'],
   rules: {
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
     ],
+    'simple-import-sort/imports': 'warn',
+    'simple-import-sort/exports': 'warn'
   },
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,24 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import react from '@vitejs/plugin-react-swc';
+import {defineConfig, loadEnv} from 'vite';
+import {checker} from 'vite-plugin-checker';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default defineConfig(({mode}) => {
+  // Load env file based on `mode` in the current working directory.
+  // Set the third parameter to '' to load all env regardless of the `VITE_` prefix.
+  const env = loadEnv(mode, process.cwd(), '');
+
+  return {
+    plugins: [
+      react(),
+      !env.VITEST
+        ? checker({
+            typescript: true,
+            eslint: {
+              lintCommand: 'eslint "./src/**/*.{ts,tsx}"',
+            },
+          })
+        : undefined,
+    ],
+  };
+});
